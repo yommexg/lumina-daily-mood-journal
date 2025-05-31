b924ea6f267b426c6fc1850b3b7e8d88cc57ca92
yommexg <hydrokidb@gmail.com>
2025-05-31 22:40:01 +0100

    Add log out to profile page

--- app/(user)/(profile)/index.tsx
+++ app/(user)/(profile)/index.tsx
@@ -3,22 +3,33 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Header } from "@/components/Header";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { View } from "react-native";
+import { primaryColor } from "@/constants/Colors";
+import { useAuthStore } from "@/store/useAuthStore";
+import { TouchableOpacity, View } from "react-native";
 
 export default function ProfileScreen() {
+  const { logout } = useAuthStore();
+
   return (
     <ThemedView style={{ flex: 1 }}>
       <SafeAreaView style={{ flex: 1 }}>
         <Header />
-        <View
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            flex: 1,
-          }}>
-          <ThemedText style={{ fontWeight: "bold", fontSize: 20 }}>
+        <View style={{ gap: 20, padding: 20 }}>
+          <ThemedText
+            style={{ fontWeight: "bold", fontSize: 20, textAlign: "center" }}>
             Profile Page
           </ThemedText>
+          <TouchableOpacity
+            onPress={logout}
+            style={{
+              backgroundColor: primaryColor,
+              paddingVertical: 10,
+              marginHorizontal: 20,
+            }}>
+            <ThemedText style={{ textAlign: "center", fontSize: 16 }}>
+              Log Out
+            </ThemedText>
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     </ThemedView>
